5ee2c6401709b61389b796bb08203609e12a8909
himanshubhatia2910 <himanshubhatia2910@gmail.com>
2023-10-19 23:02:11 +0530

    Added Javascript Notes Application

---
+++ JavaScript-Notes-Application/script.js
@@ -0,0 +1,43 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+
+}
+createBtn.addEventListener("click",()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    
+})
+
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
